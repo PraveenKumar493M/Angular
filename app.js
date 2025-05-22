b1f7f17ecfc6c73671f789565a71e3e5b96a9105
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-05-22 11:30:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ angular.module('app', ['ngRoute'])
     var records = this;
     records.items = [];
     records.newItem = {};
-
     records.fetchData = function() {
       DataService.getItems()
         .then(function(response) {
