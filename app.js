76906dd7b6e024f98c24e943ecf4047c5199b19d
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-06-20 11:06:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ angular.module('app', ['ngRoute'])
           console.error('Error fetching data:', error);
         });
     };
+    
     // Create new record
     records.createItem = function() {
       DataService.createItem(records.newItem)
