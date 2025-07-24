7f14ba48de5774d16b985b3f1e7230f976fcfaf8
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-07-24 11:39:05 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -26,7 +26,6 @@ export class TextFieldAutofillMonitorExample implements AfterViewInit, OnDestroy
       .monitor(this.lastName)
       .subscribe(e => (this.lastNameAutofilled = e.isAutofilled));
   }
-  
   ngOnDestroy() {
     this._autofill.stopMonitoring(this.firstName);
     this._autofill.stopMonitoring(this.lastName);
