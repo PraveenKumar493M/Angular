ca103efd271bc55b38294e9990b5451172e4f09f
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-08-06 11:39:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ angular.module('app', ['ngRoute'])
         redirectTo: '/'
       });
   }])
+  
   .controller('HomeController', ['$http', 'DataService', function($http, DataService) {
     var records = this;
     records.items = [];
@@ -50,3 +51,4 @@ angular.module('app', ['ngRoute'])
       return $http.post(baseUrl + '/item', item);
     };
   }]);
+
